6649622ab8c9bc0898a8ad5298b23458eb83f2c0
Fabio Luiz <64435064+devfabioluiz@users.noreply.github.com>
2026-01-14 22:22:13 -0300

    index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
 const contatosRoutes = require("./routes/contatos.js");
-require('dotenv').config();
-const connectDatabase = require('./banco-de-dados/config.js');
-
-connectDatabase();
 
 app.use("/contatos", contatosRoutes);
+
 app.get("/", (req, res) => {
   res.send("Api rodando! Coloque as rotas corretamente");
 });
